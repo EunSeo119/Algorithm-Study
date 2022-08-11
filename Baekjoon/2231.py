c36d4b67e4c25467fdf0367bfc7e2c748d746b3c
EunSeo119 <64001133+EunSeo119@users.noreply.github.com>
2022-08-11 18:11:56 +0900

    20220811

--- Baekjoon/2231.py
+++ Baekjoon/2231.py
@@ -8,3 +8,25 @@ for i in range(1, N+1):
         break
 
 print(result)
+
+
+
+# N=int(input())
+
+# check=0
+# result=[]
+
+# for i in range(1,N):
+#     sum=i
+#     str_i=str(i)
+#     for j in range(len(str_i)):
+#         sum+=int(str_i[j])
+#     if N==sum:
+#         result.append(i)
+#         check=1
+#         break
+
+# if check==0:
+#     print(0)
+# else:
+#     print(min(result))
